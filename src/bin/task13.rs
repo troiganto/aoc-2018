209b89f5fb719d7002c8d7cdce168c4353062c90
Nico Madysa <nico.madysa@tu-dresden.de>
2018-12-07 11:11:35 +0100

    Solve Day 7 Part 2.

--- src/bin/task13.rs
+++ src/bin/task13.rs
@@ -1,13 +1,28 @@
 use std::{
     collections::BTreeMap,
     io::{self, BufRead},
+    iter::{FromIterator, FusedIterator},
     str::FromStr,
 };
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Task(u8);
+
+impl Task {
+    fn get_duration(&self) -> u32 {
+        (self.0 - b'A' + 1) as u32
+    }
+}
+
+fn tasks_to_string<T: IntoIterator<Item = Task>>(tasks: T) -> String {
+    let bytes = tasks.into_iter().map(|task| task.0).collect();
+    String::from_utf8(bytes).unwrap()
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Prerequisite {
-    task: u8,
-    needed: u8,
+    task: Task,
+    needed: Task,
 }
 
 impl FromStr for Prerequisite {
@@ -20,54 +35,237 @@ impl FromStr for Prerequisite {
         let &task = s.as_bytes().get(36).ok_or(BadLine)?;
         (b'A'..=b'Z').any(|c| c == needed).ok_or(BadLine)?;
         (b'A'..=b'Z').any(|c| c == task).ok_or(BadLine)?;
-        Ok(Prerequisite { task, needed })
+        Ok(Prerequisite {
+            task: Task(task),
+            needed: Task(needed),
+        })
     }
 }
 
 #[derive(Debug)]
 struct BadLine;
 
-fn collect_prerequisites<I>(prerequisites: I) -> BTreeMap<u8, Vec<u8>>
-where
-    I: IntoIterator<Item = Prerequisite>,
-{
-    let mut result = BTreeMap::<u8, Vec<u8>>::new();
-    for Prerequisite { task, needed } in prerequisites {
-        result.entry(task).or_default().push(needed);
-        result.entry(needed).or_default();
+#[derive(Clone, Default, Debug)]
+struct PrerequisiteTable(BTreeMap<Task, Vec<Task>>);
+
+impl PrerequisiteTable {
+    fn len(&self) -> usize {
+        self.0.len()
     }
-    result
-}
 
-fn sort_tasks_by_prerequisites<I>(prerequisites: I) -> Vec<u8>
-where
-    I: IntoIterator<Item = Prerequisite>,
-{
-    use boolinator::Boolinator;
+    fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
 
-    let mut prerequisites = collect_prerequisites(prerequisites);
-    let mut result = Vec::with_capacity(prerequisites.len());
-    while !prerequisites.is_empty() {
-        let &task = prerequisites
+    fn next_doable_task(&mut self) -> Option<Task> {
+        use boolinator::Boolinator;
+
+        let next = self
+            .0
             .iter()
             .find_map(|(task, prerequisites)| prerequisites.is_empty().as_some(task))
-            .unwrap();
-        prerequisites.remove(&task);
-        result.push(task);
-        for needs in prerequisites.values_mut() {
-            needs.retain(|&n| n != task);
+            .cloned();
+        if let Some(next) = &next {
+            self.0.remove(next);
+        }
+        next
+    }
+
+    fn finish_task(&mut self, done: &Task) {
+        for needs in self.0.values_mut() {
+            needs.retain(|task| task != done);
         }
     }
-    result
+}
+
+impl FromIterator<Prerequisite> for PrerequisiteTable {
+    fn from_iter<I>(prerequisites: I) -> Self
+    where
+        I: IntoIterator<Item = Prerequisite>,
+    {
+        let mut result = BTreeMap::<Task, Vec<Task>>::new();
+        for Prerequisite { task, needed } in prerequisites {
+            result.entry(task).or_default().push(needed.clone());
+            result.entry(needed).or_default();
+        }
+        PrerequisiteTable(result)
+    }
+}
+
+impl IntoIterator for PrerequisiteTable {
+    type Item = Task;
+    type IntoIter = TaskIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        TaskIter(self)
+    }
+}
+
+struct TaskIter(PrerequisiteTable);
+
+impl Iterator for TaskIter {
+    type Item = Task;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let task = self.0.next_doable_task();
+        if let Some(task) = &task {
+            self.0.finish_task(task);
+        }
+        task
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len(), Some(self.len()))
+    }
+}
+
+impl ExactSizeIterator for TaskIter {
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+impl FusedIterator for TaskIter {}
+
+#[derive(Debug, Default, Clone)]
+struct Worker {
+    task: Option<Task>,
+    time_until_ready: u32,
+}
+
+impl Worker {
+    fn is_free(&self) -> bool {
+        self.task.is_none()
+    }
+
+    fn give_task(&mut self, task: Task, extra_time: u32) -> Result<(), IsBusy> {
+        match self.task {
+            Some(_) => Err(IsBusy),
+            None => {
+                self.time_until_ready = task.get_duration() + extra_time;
+                self.task = Some(task);
+                Ok(())
+            },
+        }
+    }
+
+    fn step(&mut self) -> Option<Task> {
+        self.time_until_ready = self.time_until_ready.saturating_sub(1);
+        match self.time_until_ready {
+            0 => self.task.take(),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct IsBusy;
+
+fn simulate_single_worker(prerequisites: PrerequisiteTable) -> String {
+    tasks_to_string(prerequisites.into_iter())
+}
+
+fn simulate_parallel_workers(
+    mut prerequisites: PrerequisiteTable,
+    workers: usize,
+    extra_time_per_task: u32,
+) -> (String, u32) {
+    let mut workers = vec![Worker::default(); workers];
+    let mut finished = Vec::new();
+    for time in 0.. {
+        if !prerequisites.is_empty() {
+            // There are remaining tasks, hand them out.
+            for worker in &mut workers {
+                if worker.is_free() {
+                    if let Some(task) = prerequisites.next_doable_task() {
+                        worker.give_task(task, extra_time_per_task).unwrap()
+                    }
+                }
+            }
+        } else {
+            // There are no remaining tasks, wait for workers to finish.
+            if workers.iter().all(|w| w.is_free()) {
+                return (tasks_to_string(finished), time);
+            }
+        }
+        // Have them perform one unit of work.
+        for worker in &mut workers {
+            if let Some(task) = worker.step() {
+                prerequisites.finish_task(&task);
+                finished.push(task);
+            }
+        }
+    }
+    panic!("overflow");
 }
 
 fn main() {
     let stdin = io::stdin();
-    let tasks = sort_tasks_by_prerequisites(
-        stdin
-            .lock()
-            .lines()
-            .map(|line| line.unwrap().parse().unwrap()),
+    let tasks = stdin
+        .lock()
+        .lines()
+        .map(|line| line.unwrap().parse().unwrap())
+        .collect::<PrerequisiteTable>();
+    println!(
+        "single worker order:   {}",
+        simulate_single_worker(tasks.clone())
     );
-    println!("correct order: {}", String::from_utf8(tasks).unwrap());
+    let (tasks, time) = simulate_parallel_workers(tasks, 5, 60);
+    println!("parallel worker order: {}", tasks);
+    println!("parallel worker time: {}", time);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn example_data() -> PrerequisiteTable {
+        vec![
+            Prerequisite {
+                task: Task(b'A'),
+                needed: Task(b'C'),
+            },
+            Prerequisite {
+                task: Task(b'F'),
+                needed: Task(b'C'),
+            },
+            Prerequisite {
+                task: Task(b'B'),
+                needed: Task(b'A'),
+            },
+            Prerequisite {
+                task: Task(b'D'),
+                needed: Task(b'A'),
+            },
+            Prerequisite {
+                task: Task(b'E'),
+                needed: Task(b'B'),
+            },
+            Prerequisite {
+                task: Task(b'E'),
+                needed: Task(b'D'),
+            },
+            Prerequisite {
+                task: Task(b'E'),
+                needed: Task(b'F'),
+            },
+        ]
+        .into_iter()
+        .collect::<PrerequisiteTable>()
+    }
+
+    #[test]
+    fn test_sequential() {
+        let prerequisites = example_data();
+        let tasks = simulate_single_worker(prerequisites);
+        assert_eq!(tasks, "CABDFE");
+    }
+
+    #[test]
+    fn test_parallel() {
+        let prerequisites = example_data();
+        let (tasks, time) = simulate_parallel_workers(prerequisites, 2, 0);
+        assert_eq!(tasks, "CABFDE");
+        assert_eq!(time, 15);
+    }
 }
